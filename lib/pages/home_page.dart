75ac64afaf0a6e85f15d5dfb009936ce2240aa6d
omkar <pendkalkar.omkar786@gmail.com>
2023-07-10 12:34:38 +0530

    nothing new added...fixed some syntax bug

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:ui';
 import 'package:intl/intl.dart';
 import 'package:geolocator/geolocator.dart';
